53107eb58e018b6df8e4e537ab13ac28cbae2fd8
Pamuda Jayathilaka <pamudasankalpa22us@gmail.com>
2025-11-14 16:59:58 +0530

    feat(CRUD): Implement 'Create' - add new book form and POST route

--- index.js
+++ index.js
@@ -7,25 +7,54 @@ const app = express();
 const port = 3000;
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "mybooks",
-  password: process.env.dbPassword,
-  port: 5432,
+    user: "postgres",
+    host: "localhost",
+    database: "mybooks",
+    password: process.env.dbPassword,
+    port: 5432,
 });
 
-db.connect();
+db.connect()
+    .then(() => console.log("Connected to database"))
+    .catch((err) => console.error("Database connection error:", err));;
+
 
 // find static files(CSS, Image)
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 //find the reason for this line
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
     res.render("index");
+});
+
+app.get("/new", (req, res) => {
+    res.render("new");
+});
+
+app.post("/add", async (req, res) => {
+    const title = req.body.title;
+    const author = req.body.author;
+    const isbn = req.body.isbn;
+    const rating = parseInt(req.body.rating);
+    const dateRead = req.body.date_read;
+    const notes = req.body.notes;
+
+    try {
+        // We use $1, $2, etc. to prevent SQL Injection
+        const query = `
+        INSERT INTO books (title, author, isbn, rating, date_read, notes)
+        VALUES ($1, $2, $3, $4, $5, $6)
+        `;
+        
+        await db.query(query, [title, author, isbn, rating, dateRead, notes]);
+        res.redirect("/")
+    } catch (err){
+        console.error("Error adding book:", err);
+    }
 })
 
-app.listen(port, ()=> {
+app.listen(port, () => {
     console.log(`Sever running on http://localhost:${port}`);
 });
