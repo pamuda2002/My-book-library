d9ecdf7c745927d3e6b3b144ecd56ecb0a736ec1
Pamuda Jayathilaka <pamudasankalpa22us@gmail.com>
2025-11-15 23:12:41 +0530

    feat(CRUD): Implement 'Update' and 'Delete' functionality

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.get("/", async (req, res) => {
         const result = await db.query("SELECT * FROM books");
 
         const books = result.rows;
-        res.render("index", {books: books});
-    }catch(err){
+        res.render("index", { books: books });
+    } catch (err) {
         console.error("Error fetching books:", err);
     }
 });
@@ -54,13 +54,52 @@ app.post("/add", async (req, res) => {
         INSERT INTO books (title, author, isbn, rating, date_read, notes)
         VALUES ($1, $2, $3, $4, $5, $6)
         `;
-        
+
         await db.query(query, [title, author, isbn, rating, dateRead, notes]);
         res.redirect("/")
-    } catch (err){
+    } catch (err) {
         console.error("Error adding book:", err);
     }
-})
+});
+
+app.get("/edit/:id", async (req, res) => {
+    const id = req.params.id;
+    try {
+        const result = await db.query("SELECT * FROM books WHERE id = $1", [id]);
+        const book = result.rows[0]; // Get the first (and only) book
+        res.render("edit.ejs", { book: book });
+    } catch (err) {
+        console.error("Error fetching book for edit:", err);
+    }
+
+});
+
+app.post("/update", async (req, res) => {
+  const id = req.body.bookId;
+  const { title, author, isbn, rating, date_read, notes } = req.body;
+
+  try {
+    const query = `
+      UPDATE books
+      SET title = $1, author = $2, isbn = $3, rating = $4, date_read = $5, notes = $6
+      WHERE id = $7
+    `;
+    await db.query(query, [title, author, isbn, rating, date_read, notes, id]);
+    res.redirect("/");
+  } catch (err) {
+    console.error("Error updating book:", err);
+  }
+});
+
+app.post("/delete", async (req, res) => {
+    const id = req.body.bookID;
+    try {
+        await db.query("DELETE FROM books WHERE id = $1", [id]);
+        res.redirect("/");
+    } catch (err) {
+        console.error("Error deleting book:", err);
+    }
+});
 
 app.listen(port, () => {
     console.log(`Sever running on http://localhost:${port}`);
